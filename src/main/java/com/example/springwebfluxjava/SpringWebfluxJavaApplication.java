f01180309f00d7260184fb1b6421beb19e451064
yoonsik <ys.shin@okestro.com>
2025-01-19 15:33:23 +0900

    initialize

---
+++ src/main/java/com/example/springwebfluxjava/SpringWebfluxJavaApplication.java
@@ -0,0 +1,14 @@
+package com.example.springwebfluxjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxJavaApplication.class, args);
+        System.out.println("start!");
+    }
+
+}
---
+++ src/test/java/com/example/springwebfluxjava/SpringWebfluxJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
